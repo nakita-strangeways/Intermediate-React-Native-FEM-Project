b0720e86c453201b675b76a82396fc3dfd740770
Nakita Strangeways <40547453+nakita-strangeways@users.noreply.github.com>
2024-04-19 14:10:42 -0700

    Up to UI Elements Recap Styling complete

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+// whenever you need to use a colour in your styles, import theme into your component and use the theme value. This allows us to swap out colours globally and ensure we always have a good overview of all the colours used in our application.
+
+export const theme = {
+  colorPurple: '#454C73',
+  colorWhite: '#fff',
+
+  // for creating contants such as colors, spacing, fonts and any UI elements you tend to use frequently.
+  space10: 10,
+  space12: 12,
+};
