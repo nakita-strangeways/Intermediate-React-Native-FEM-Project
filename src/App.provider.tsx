9329d46db0744c6ecc7c0758d58e48ac152ddbec
Nakita Strangeways <40547453+nakita-strangeways@users.noreply.github.com>
2024-04-26 20:44:44 -0700

    addin delete

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, {
+  createContext,
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import { MoodOptionType, MoodOptionWithTimestamp } from './types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -39,12 +46,22 @@ const defaultValue = {
   handleDeleteMood: () => {},
 };
 
-const AppContext = React.createContext<AppContextType>(defaultValue);
+const AppContext = createContext<AppContextType>(defaultValue);
 
-export const AppProvider: React.FC = ({ children }) => {
-  const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
+export const AppProvider: FC = ({ children }) => {
+  const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
 
-  const handleSelectMood = React.useCallback((mood: MoodOptionType) => {
+  const handleDeleteMood = useCallback((mood: MoodOptionWithTimestamp) => {
+    setMoodList(current => {
+      const newValue = current.filter(
+        item => item.timestamp !== mood.timestamp,
+      );
+      setAppData({ moods: newValue });
+      return newValue;
+    });
+  }, []);
+
+  const handleSelectMood = useCallback((mood: MoodOptionType) => {
     setMoodList(current => {
       const newValue = [...current, { mood, timestamp: Date.now() }];
       setAppData({ moods: newValue });
@@ -52,7 +69,7 @@ export const AppProvider: React.FC = ({ children }) => {
     });
   }, []);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const getDataFromStorage = async () => {
       const data = await getAppData();
 
@@ -64,10 +81,11 @@ export const AppProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AppContext.Provider value={{ moodList, handleSelectMood }}>
+    <AppContext.Provider
+      value={{ moodList, handleSelectMood, handleDeleteMood }}>
       {children}
     </AppContext.Provider>
   );
 };
 
-export const useAppContext = () => React.useContext(AppContext);
+export const useAppContext = () => useContext(AppContext);
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
+// import { StyleSheet } from 'react-native';
 import { BottomTabsNavigator } from './screens/BottomTabs.navigator';
 import { AppProvider } from './App.provider';
+import { Platform, UIManager } from 'react-native';
+
+// LayoutAnimation is enabled by default on iOS, but it's still experimental on Android. In order to enable LayoutAnimation on Android
+if (Platform.OS === 'android') {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
 
 export const App: FC = () => {
   return (
@@ -14,11 +22,11 @@ export const App: FC = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'teal',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'teal',
+//   },
+// });
 
 export default App;
